ce361f41b09cf1123fec77c308553a5ed15ca09d
donnymboma01 <donnymboma01@gmail.com>
2022-04-28 03:44:47 +0200

    user model

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+
+    name :{
+        type:String,
+        required :[true,'Please add a name !']
+    },
+    email :{
+        type:String,
+        required:[true,'Please enter you email !'],
+        unique :true
+    },
+    password :{
+        type:String,
+        required:[true,'Please enter you password !']
+    }
+},{
+    timestamps :true
+});
+
+const user = mongoose.model('user',UserSchema);
+
+module.exports = user;
