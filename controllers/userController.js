0cad2450c4f21d1a762e734dda7ea3654e08613c
donnymboma01 <donnymboma01@gmail.com>
2022-04-28 04:46:33 +0200

    login users done

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,8 +6,6 @@ const UserModel = require('../models/userModel');
 
 
 const registerUser = asyncHandler( async(req,res) =>{
-    //console.log(req.body);
-    //res.status(200).json({ message :'Register a new user ' });
 
     const { name, email, password } = req.body;
 
@@ -15,18 +13,74 @@ const registerUser = asyncHandler( async(req,res) =>{
         res.status(400);
         throw new Error('Please add all fields !');
     }
+
+    const userExists = await UserModel.findOne({ email });
+
+    if(userExists){
+        res.status(400);
+        throw new Error('User already exists !');
+    }
+
+    //hash the password.
+    const salt = await Bcrypt.genSalt(10);
+    const hashedPassword = await Bcrypt.hash(password,salt);
+
+    const user = await UserModel.create({
+        name : name, 
+        email : email,
+        password:hashedPassword
+    });
+    if(user){
+        res.status(201).json({
+            _id : user._id,
+            name :user.name,
+            email :user.email,
+            password :hashedPassword
+        })
+    }else{
+        res.status(400);
+        throw new Error('Invalid user datas !');
+    }
+
+    res.status(201).json({ message :  'A new user is added !' })
 });
 
 
+
 const loginUser = asyncHandler( async(req,res) =>{
-    //console.log(req.body);
-    res.status(200).json({ message :'Login user ' });
+    
+    const { email , password } = req.body;
+
+    const user = await UserModel.findOne({ email });
+
+    if(user && (await Bcrypt.compare(password,user.password))){
+        res.status(200).json({
+            _id : user._id,
+            name :user.name,
+            email :user.email,
+            token : generateToken(user.id,user.name,user.email)
+        })
+    }else{
+        res.status(404); 
+        throw new Error('Invalid creadentials !');
+    }
 });
 
 
 const getMe = asyncHandler(async (req,res) =>{
     res.status(200).json({ message :'Yeah, this is actually me !' });
 });
+
+
+const generateToken = (id,name,email) =>{
+    return jwt.sign({
+        id,
+        name,
+        email
+    },process.env.SECRET_JWT_TOKEN,{
+        expiresIn:'1d'
+    })
+};
 
 
 module.exports = {
